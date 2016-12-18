2b886c7146668e121d1d4df3bb28554700102c5a
Vladimir Rutsky <vladimir@rutsky.org>
2016-12-19 00:47:28 +0300

    add untested and probably not working WS endpoint

--- src/testing_server/server.py
+++ src/testing_server/server.py
@@ -1,14 +1,24 @@
+import asyncio
 import logging
+import json
 
+import aiohttp
 import aiohttp.hdrs
-import aiohttp.web
+from aiohttp import web
+from aiohttp import WSMsgType
 import aiohttp_cors
+import async_timeout
 
 from . import abc
 from .jsend import JSendFail, jsend_handler
 from .auth_mixin import AuthMixin, requires_login
 
 __all__ = ('Server',)
+
+_PING_INTERVAL = 30
+_WS_AUTH_TIMEOUT = 30
+
+_logger = logging.getLogger(__name__)
 
 
 class Server(AuthMixin):
@@ -26,6 +36,8 @@ class Server(AuthMixin):
         self._credentials_checker = credentials_checker
         self._loop = loop
         self._enable_cors = enable_cors
+
+        self._websockets = set()
 
     async def start(self):
         if self._enable_cors:
@@ -51,6 +63,9 @@ class Server(AuthMixin):
             api_prefix + '/check_token', self.get_check_token))
 
         self._app.router.add_get(
+            api_prefix + '/ws', self.get_ws)
+
+        self._app.router.add_get(
             '/users', self.handler_not_implemented)
         self._app.router.add_get(
             '/users/{username}/{home_assignment}/{revision}/',
@@ -59,7 +74,11 @@ class Server(AuthMixin):
         self._app.router.add_get('/ws', self.handler_not_implemented)
 
     async def stop(self):
-        pass
+        while self._websockets:
+            ws = self._websockets.pop()
+            await ws.close(
+                code=aiohttp.WSCloseCode.GOING_AWAY,
+                message='Server shutdown')
 
     async def _json_body(self, request):
         ctype = request.headers.get(aiohttp.hdrs.CONTENT_TYPE, '').lower()
@@ -84,7 +103,7 @@ class Server(AuthMixin):
         return "Token is valid."
 
     @jsend_handler
-    async def post_login(self, request: aiohttp.web.Request):
+    async def post_login(self, request: web.Request):
         json_body = await self._json_body(request)
 
         if not isinstance(json_body, dict):
@@ -112,6 +131,132 @@ class Server(AuthMixin):
         token = (await self.token_provider.generate_token(login)).decode()
 
         return token
+
+    async def get_ws(self, request: web.Request):
+        ws = web.WebSocketResponse()
+        await ws.prepare(request)
+
+        self._websockets.add(ws)
+
+        async def send_pings(ws: web.WebSocketResponse,
+                             ping_interval=_PING_INTERVAL):
+            while True:
+                try:
+                    await ws.ping()
+
+                except asyncio.CancelledError:
+                    return
+
+                except RuntimeError:
+                    _logger.exception(
+                        "Exception during ws.ping(). "
+                        "May be closing web socket? Skipping...")
+
+                try:
+                    await asyncio.sleep(ping_interval)
+                except asyncio.CancelledError:
+                    return
+
+        ping_sender_task = self._loop.create_task(send_pings(ws))
+
+        def get_msg_payload(msg):
+            assert msg.tp == WSMsgType.TEXT
+            try:
+                json_body = json.loads(msg.data)
+            except json.JSONDecodeError:
+                _logger.exception(
+                    "Can't decode JSON from web socket: {!r}".format(
+                        msg.data
+                    ))
+                return None
+
+            required_keys = frozenset(['token', 'type'])
+            if required_keys.difference(frozenset(json_body.keys())):
+                _logger.error(
+                    "Web socket message doesn't have all required fields: "
+                    "{!r}".format(json_body))
+                return None
+
+            return json_body
+
+        try:
+            try:
+                with async_timeout.timeout(_WS_AUTH_TIMEOUT, loop=self._loop):
+                    auth_msg = await ws.receive()
+            except TimeoutError:
+                _logger.error(
+                    "Timeout obtaining authentication message in WS. "
+                    "Closing WS.")
+                return ws
+
+            if auth_msg.type != WSMsgType.TEXT:
+                _logger.error(
+                    "Authentication WS message has type {!r}, "
+                    "but expected {!r}. Closing WS.".format(
+                        auth_msg.type, WSMsgType.TEXT))
+                return ws
+
+            auth_data = get_msg_payload(auth_msg)
+            if auth_data is None:
+                return ws
+
+            auth_type_string = 'AUTH'
+
+            if auth_data['type'] != auth_type_string:
+                _logger.error(
+                    "Authentication WS message payload has type field {!r}, "
+                    "but expected {!r}. Closing WS.".format(
+                        auth_data['type'], auth_type_string))
+                return ws
+
+            payload = self.token_provider.validate_token(auth_data['token'])
+            if payload is None:
+                _logger.error(
+                    "Token sent over WS is invalid.")
+                await ws.send_json({
+                    'status': 'fail',
+                    'data': {
+                        'message': 'Invalid token.',
+                    },
+                })
+                return ws
+            else:
+                _logger.info(
+                    "User {!r} authenticated.".format(payload['login']))
+                await ws.send_json({
+                    'status': 'success',
+                    'message': 'Token is valid.',
+                })
+
+            async for msg in ws:
+                if msg.tp == WSMsgType.TEXT:
+                    json_body = get_msg_payload(msg)
+                    if json_body is None:
+                        continue
+
+                    _logger.debug("Got Web Socket message: {!r}".format(
+                        json_body))
+
+                elif msg.tp == aiohttp.WSMsgType.ERROR:
+                    _logger.error(
+                        "Got an exception in web socket",
+                        exc_info=ws.exception())
+
+                else:
+                    _logger.error(
+                        "Unhandled web socket message with type {!r}".format(
+                            msg.tp))
+
+        finally:
+            ping_sender_task.cancel()
+            await ping_sender_task
+
+            await ws.close()
+
+            # May be already removed is server is shutting down.
+            self._websockets.discard(ws)
+
+        return ws
 
     @jsend_handler
     async def handler_not_implemented(self, request):
